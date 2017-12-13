4bae76216bca1d67629446394977e9341e7a8f58
Wilker Ribeiro <rekliw97@gmail.com>
2017-12-13 19:50:06 -0200

    Staging

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # calcAPI
+Uma _API_ que retorna resultados de operações matemáticas;
+## Operações
+As operações são dadas na url no seguinte formato:
+```
+domain.com/{operação}/{primeiro_valor}/{segundo_valor}
+```
+e elas são:
+### Soma
+A soma pode ser acionada pelas seguintes strings:
++ ```+```
++ ```soma```
++ ```plus```
++ ```add```
+### Subtração
+A soma pode ser acionada pelos seguintes nomes:
++ ```-```
++ ```sub```
++ ```menos```
++ ```minus```
++ ```less```
+### Multiplicação
+A soma pode ser acionada pelos seguintes nomes:
++ ```*```
++ ```multi```
++ ```mult```
++ ```times```
+### Divisão
+A soma pode ser acionada pelos seguintes nomes:
++ ```:```
++ ```div```
++ ```take```
++ ```split```
+### Raiz
+A soma pode ser acionada pelos seguintes nomes:
++ ```√```
++ ```root```
++ ```raiz```
+### Esponencial
+A soma pode ser acionada pelos seguintes nomes:
++ ```^```
++ ```exp```
+
+# todo:
+
++ Aceitar nth valores;
--- routes/calc.js
+++ routes/calc.js
@@ -3,9 +3,26 @@ let router = require('express').Router();
 router.get('/:calc/:x/:y', (req, res) => {
 	console.log(req.params)
 
+	var z = {};
+
+	// console.log('x int: ', req.params.x.split('.')[0].length)
+	// console.log('x dot: ', req.params.x.split('.')[1].length)
+	// console.log('y int: ', req.params.y.split('.')[0].length)
+	// console.log('y dot: ', req.params.y.split('.')[1].length)
+
+	// console.log(req.params.x.split('.')[0].length / req.params.x.split('.')[1].length)
+	// console.log(req.params.y.split('.')[0].length / req.params.y.split('.')[1].length)
+
+	if (req.params.x.length > 16 ||
+		req.params.y.length > 16)
+		z.err = 'Params given are too high, the result is not going to be too accurate'
+
 	var x = parseFloat(req.params.x)
 	var y = parseFloat(req.params.y)
 	var calc = req.params.calc
+
+	console.log(x)
+	console.log(y)
 
 	if (isNaN(x) || isNaN(y)) {
 		return res.send({
@@ -13,44 +30,46 @@ router.get('/:calc/:x/:y', (req, res) => {
 		})
 	}
 
-	var result = new Number();
 	switch (calc) {
 		case '+':
 		case 'soma':
 		case 'plus':
 		case 'add':
-			result = x + y;
+			z.result = x + y;
 			break;
 		case '-':
 		case 'sub':
+		case 'menos':
 		case 'minus':
 		case 'less':
-			result = x - y;
+			z.result = x - y;
 			break;
 		case '*':
 		case 'multi':
 		case 'mult':
 		case 'times':
-			result = x * y;
+			z.result = x * y;
 			break;
 		case ':':
 		case 'div':
 		case 'take':
 		case 'split':
-			result = x / y;
+			z.result = x / y;
 			break;
 		case '√':
-		case 'sqrt':
-			result = Math.pow(x, 1 / y);
+		case 'root':
+		case 'raiz':
+			z.result = Math.pow(x, 1 / y);
+			break;
+		case '^':
+		case 'exp':
+			z.result = Math.pow(x, y);
 			break;
 		default:
-			return res.send({
-				err: 'Operation \'' + calc + '\' not known'
-			})
+			z.err = 'Operation \'' + calc + '\' not known';
 	}
-	res.send({
-		result
-	})
+	console.log
+	res.send(z)
 })
 
 module.exports = router;
