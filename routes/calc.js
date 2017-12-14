156b4129ac87ec70b29044f5b8fc4f23cc57662c
Wilker Ribeiro <rekliw97@gmail.com>
2017-12-13 22:57:14 -0200

    Staging

--- routes/calc.js
+++ routes/calc.js
@@ -15,7 +15,7 @@ router.get('/:calc/:x/:y', (req, res) => {
 
 	if (req.params.x.length > 16 ||
 		req.params.y.length > 16)
-		z.err = 'Params given are too high, the result is not going to be too accurate'
+		z.wrn = 'Params given are too high, the result is not going to be too accurate.'
 
 	var x = parseFloat(req.params.x)
 	var y = parseFloat(req.params.y)
@@ -26,7 +26,7 @@ router.get('/:calc/:x/:y', (req, res) => {
 
 	if (isNaN(x) || isNaN(y)) {
 		return res.send({
-			err: 'Params given are not number type'
+			err: 'Params given are not number type.'
 		})
 	}
 
@@ -66,9 +66,71 @@ router.get('/:calc/:x/:y', (req, res) => {
 			z.result = Math.pow(x, y);
 			break;
 		default:
-			z.err = 'Operation \'' + calc + '\' not known';
+			z.err = 'Operation \'' + calc + '\' not known.';
 	}
-	console.log
+//	console.log
+	res.send(z)
+})
+router.get('/long/:calc/:x/:y', (req, res) => {
+	console.log(req.params)
+	
+	var BigNumber = require('bignumber.js');
+	var z = {};
+
+	if (isNaN(parseFloat(req.params.x)) || isNaN(parseFloat(req.params.y))) {
+		return res.send({
+			err: 'Params given are not number type.'
+		})
+	}
+	
+	var x = new BigNumber(req.params.x)
+	var y = new BigNumber(req.params.y)
+	var aux = new BigNumber(1)
+	var calc = req.params.calc
+
+	console.log(x)
+	console.log(y)
+
+	switch (calc) {
+		case '+':
+		case 'soma':
+		case 'plus':
+		case 'add':
+			z.wrn = 'The result is an string. You shold parse it.'
+			z.result = x.plus(y); // + y;
+			break;
+		case '-':
+		case 'sub':
+		case 'menos':
+		case 'minus':
+		case 'less':
+			z.result = x - y;
+			break;
+		case '*':
+		case 'multi':
+		case 'mult':
+		case 'times':
+			z.result = x * y;
+			break;
+		case ':':
+		case 'div':
+		case 'take':
+		case 'split':
+			z.result = x / y;
+			break;
+		case '√':
+		case 'root':
+		case 'raiz':
+			z.result = Math.pow(x, 1 / y);
+			break;
+		case '^':
+		case 'exp':
+			z.result = x.pow(y);
+			break;
+		default:
+			z.err = 'Operation \'' + calc + '\' not known.';
+	}
+	console.log(z)
 	res.send(z)
 })
 
