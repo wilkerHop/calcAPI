c7547fa890c3bb0b9105e28ed8d8c30a626a824d
Wilker Ribeiro <rekliw97@gmail.com>
2017-12-17 02:44:07 -0200

    Hot-fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
+let cors = require('cors');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -12,7 +13,15 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
 	res.sendStatus(404)
 })
- 
+
+app.use(cors({
+	origin: "*",
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	preflightContinue: false,
+	optionsSuccessStatus: 204,
+	allowedHeaders: ['Origin', 'X-Requested-With', 'xx-access-token', 'Content-Type', 'Accept']
+}))
+
 var calc = require('./routes/calc');
 app.use('/', calc);
 
